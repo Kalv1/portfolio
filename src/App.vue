11181c7e94d2e907ac79ff5079009f4b8e0d6dcf
Kalv1 <calvin.lambert@protonmail.com>
2023-05-20 21:17:52 +0200

    :iphone: Fix span display on mobile & add links

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,36 @@
   <img class="absolute -left-[800px] -top-[750px]" src="./assets/img/Rectangle.png" />
   <div class="container mx-auto relative text-white px-5">
     <Navbar />
-    <div class="my-16 md:my-72 flex justify-center px-16 lg:px-64">
+    <div class="my-16 md:my-72 flex justify-center px-8 md:px-16 lg:px-64">
       <div class="w-full flex justify-center gap-5 flex-col text-center">
         <h1 class="text-5xl md:text-7xl font-extrabold">Calvin <span
-            class="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#77009F] to-[#00C3FF] bg-[length:400%_400%]">LAMBERT</span>
+            class="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#77009F] to-[#00C3FF] bg-[length:400%_400%] block md:inline-block">LAMBERT</span>
         </h1>
         <p class="text-md md:text-2xl font-regular leading-relaxed">Hi! I am a 22 years old front-end developer mainly
           focused on
           VueJS & NuxtJS. I'm
           also interested in the world of 3D on the web and more specifically in THREEJS</p>
         <div class="flex justify-center items-center gap-4">
-          <button class="links rounded-full bg-white p-2">
-            <img class="w-6 h-6" src="./assets/img/github.png" alt="github">
-          </button>
-          <button class="links rounded-full bg-white p-2">
-            <img class="w-6 h-6" src="./assets/img/twitter.png" alt="github">
-          </button>
-          <button class="links rounded-full bg-white p-2">
-            <img class="w-6 h-6" src="./assets/img/email.png" alt="github">
-          </button>
-          <button class="links rounded-full bg-white p-2">
-            <img class="w-6 h-6" src="./assets/img/linkedIn.png" alt="github">
-          </button>
+          <a href="https://github.com/Kalv1">
+            <button class="links rounded-full bg-white p-2">
+              <img class="w-6 h-6" src="./assets/img/github.png" alt="github">
+            </button>
+          </a>
+          <a href="https://twitter.com/Calv1_">
+            <button class="links rounded-full bg-white p-2">
+              <img class="w-6 h-6" src="./assets/img/twitter.png" alt="github">
+            </button>
+          </a>
+          <a href="mailto:calvin.lambert@protonmail.com">
+            <button class="links rounded-full bg-white p-2">
+              <img class="w-6 h-6" src="./assets/img/email.png" alt="github">
+            </button>
+          </a>
+          <a href="https://www.linkedin.com/in/calvin-lambert/">
+            <button class="links rounded-full bg-white p-2">
+              <img class="w-6 h-6" src="./assets/img/linkedIn.png" alt="github">
+            </button>
+          </a>
         </div>
       </div>
     </div>
@@ -70,21 +78,24 @@
     <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
       <Card>
         <h2 class="text-lg md:text-xl font-bold">Front-end</h2>
-        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I can build interfaces with VueJS and TailwindCSS. I also master
+        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I can build interfaces with VueJS and
+          TailwindCSS. I also master
           different UI framework such as
           Ant Design and Buefy. I also have some experience with ReactJS!</p>
         <img class="h-32 absolute -bottom-5 -right-5 opacity-25" src="./assets/img/js.png" alt="js">
       </Card>
       <Card>
         <h2 class="text-lg md:text-xl font-bold">Back-end</h2>
-        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I have notions in back-end thanks to the studies I followed. I can
+        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I have notions in back-end thanks to the
+          studies I followed. I can
           build backend applications via
           Laravel and I also experiment on NestJS.</p>
         <img class="h-48 absolute -bottom-12 -right-10 opacity-25" src="./assets/img/php.png" alt="js">
       </Card>
       <Card>
         <h2 class="text-lg md:text-xl font-bold">UI/UX Design</h2>
-        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I also have skills in UI/UX design. I use software such as Lunacy /
+        <p class="mt-3 text-sm md:text-base tracking-wide leading-relaxed">I also have skills in UI/UX design. I use
+          software such as Lunacy /
           Figma and I have also in my
           past used software such as Illustrator and Photoshop.</p>
         <img class="h-32 absolute -bottom-5 -right-5 opacity-25" src="./assets/img/figma.svg" alt="js">
@@ -122,7 +133,7 @@
       </Card>
     </div>
   </div>
-  <div class="h-[1px] bg-gray-800 mt-10"/>
+  <div class="h-[1px] bg-gray-800 mt-10" />
   <div class="py-6 md:py-10 container mx-auto bg-[#161616] text-white text-center text-sm md:text-base">
     <p>Made with ❤️ by Calvin.</p>
   </div>
